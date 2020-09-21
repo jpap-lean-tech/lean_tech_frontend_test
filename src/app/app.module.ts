201f82ec78ed51ef138049010470d095b4595622
juan arteaga <juanarteaga@MacBook-Pro-de-juan.local>
2020-09-21 08:32:07 -0500

    toolbar_design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Componnets
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main/main.component';
+import { HeaderComponent } from './main/header/header.component';
+
+// Modules
+import { MatIconModule, MatToolbarModule, MatDialogModule, MatPaginatorModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
